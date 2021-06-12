696d25bfa31dd1f7f1eda5e4c31528a1a9de3828
lethikimoanh21a4040085 <oanhha218@gmail.com>
2021-06-12 13:05:26 +0700

    First commit

---
+++ Okono_Mmanagement/FrmPhieuChotCa.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Okono
+{
+    partial class FrmPhieuChotCa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPhieuChotCa));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbThoiGian = new System.Windows.Forms.TextBox();
+            this.tbTenNhanVien = new System.Windows.Forms.TextBox();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.cbMaNhanVien = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.gridViewChenhLech = new System.Windows.Forms.DataGridView();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.tbTienThucTe = new System.Windows.Forms.TextBox();
+            this.cbCaLamViec = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewChenhLech)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(30)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(513, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PHIẾU CHỐT CA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(640, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ngày:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.label3.Location = new System.Drawing.Point(141, 283);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mã nhân viên:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.label4.Location = new System.Drawing.Point(136, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tên nhân viên:";
+            // 
+            // tbThoiGian
+            // 
+            this.tbThoiGian.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.tbThoiGian.Location = new System.Drawing.Point(697, 182);
+            this.tbThoiGian.Name = "tbThoiGian";
+            this.tbThoiGian.Size = new System.Drawing.Size(226, 28);
+            this.tbThoiGian.TabIndex = 14;
+            // 
+            // tbTenNhanVien
+            // 
+            this.tbTenNhanVien.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.tbTenNhanVien.Location = new System.Drawing.Point(285, 340);
+            this.tbTenNhanVien.Name = "tbTenNhanVien";
+            this.tbTenNhanVien.Size = new System.Drawing.Size(226, 28);
+            this.tbTenNhanVien.TabIndex = 14;
+            // 
+            // btnIn
+            // 
+            this.btnIn.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(30)))), ((int)(((byte)(0)))));
+            this.btnIn.Image = ((System.Drawing.Image)(resources.GetObject("btnIn.Image")));
+            this.btnIn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnIn.Location = new System.Drawing.Point(416, 630);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(95, 40);
+            this.btnIn.TabIndex = 30;
+            this.btnIn.Text = "  In";
+            this.btnIn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIn.UseVisualStyleBackColor = true;
+            this.btnIn.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // cbMaNhanVien
+            // 
+            this.cbMaNhanVien.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.cbMaNhanVien.FormattingEnabled = true;
+            this.cbMaNhanVien.Location = new System.Drawing.Point(285, 275);
+            this.cbMaNhanVien.Name = "cbMaNhanVien";
+            this.cbMaNhanVien.Size = new System.Drawing.Size(226, 28);
+            this.cbMaNhanVien.TabIndex = 24;
+            this.cbMaNhanVien.SelectedIndexChanged += new System.EventHandler(this.cbMaNhanVien_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.label7.Location = new System.Drawing.Point(693, 343);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Thực thu:";
+            // 
+            // gridViewChenhLech
+            // 
+            this.gridViewChenhLech.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewChenhLech.Location = new System.Drawing.Point(140, 402);
+            this.gridViewChenhLech.Name = "gridViewChenhLech";
+            this.gridViewChenhLech.RowHeadersWidth = 51;
+            this.gridViewChenhLech.Size = new System.Drawing.Size(963, 188);
+            this.gridViewChenhLech.TabIndex = 32;
+            this.gridViewChenhLech.Text = "dataGridView1";
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(30)))), ((int)(((byte)(0)))));
+            this.btnHuy.Image = ((System.Drawing.Image)(resources.GetObject("btnHuy.Image")));
+            this.btnHuy.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnHuy.Location = new System.Drawing.Point(715, 630);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(95, 40);
+            this.btnHuy.TabIndex = 33;
+            this.btnHuy.Text = "  Huỷ";
+            this.btnHuy.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHuy.UseVisualStyleBackColor = true;
+            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(30)))), ((int)(((byte)(0)))));
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.Location = new System.Drawing.Point(1008, 630);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(95, 40);
+            this.btnThoat.TabIndex = 34;
+            this.btnThoat.Text = " Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnXem
+            // 
+            this.btnXem.Font = new System.Drawing.Font("Times New Roman", 13.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.ForeColor = System.Drawing.Color.Red;
+            this.btnXem.Image = global::Okono_Mmanagement.Properties.Resources.Background__1_1;
+            this.btnXem.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXem.Location = new System.Drawing.Point(140, 630);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(95, 40);
+            this.btnXem.TabIndex = 44;
+            this.btnXem.Text = "  Xem";
+            this.btnXem.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // tbTienThucTe
+            // 
+            this.tbTienThucTe.Font = new System.Drawing.Font("Times New Roman", 13.2F);
+            this.tbTienThucTe.Location = new System.Drawing.Point(779, 340);
+            this.tbTienThucTe.Name = "tbTienThucTe";
+            this.tbTienThucTe.Size = new System.Drawing.Size(250, 28);
+            this.tbTienThucTe.TabIndex = 45;
+            // 
+            // cbCaLamViec
+            // 
+            this.cbCaLamViec.FormattingEnabled = true;
+            this.cbCaLamViec.Location = new System.Drawing.Point(357, 183);
+            this.cbCaLamViec.Name = "cbCaLamViec";
+            this.cbCaLamViec.Size = new System.Drawing.Size(250, 27);
+            this.cbCaLamViec.TabIndex = 46;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(319, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 19);
+            this.label5.TabIndex = 47;
+            this.label5.Text = "Ca:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1044, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 19);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "VNĐ";
+            // 
+            // FrmPhieuChotCa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1284, 781);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbCaLamViec);
+            this.Controls.Add(this.tbTienThucTe);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.gridViewChenhLech);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbMaNhanVien);
+            this.Controls.Add(this.btnIn);
+            this.Controls.Add(this.tbTenNhanVien);
+            this.Controls.Add(this.tbThoiGian);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPhieuChotCa";
+            this.Text = "Phiếu chốt ca";
+            this.Load += new System.EventHandler(this.FrmPhieuChotCa_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewChenhLech)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbThoiGian;
+        private System.Windows.Forms.TextBox tbTenNhanVien;
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.ComboBox cbMaNhanVien;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView gridViewChenhLech;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.TextBox tbTienThucTe;
+        private System.Windows.Forms.ComboBox cbCaLamViec;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Okono_Mmanagement/FrmPhieuChotCa.cs
@@ -0,0 +1,181 @@
+﻿using Okono_Mmanagement;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using COMExcel = Microsoft.Office.Interop.Excel;
+
+namespace Okono
+{
+    public partial class FrmPhieuChotCa : Form
+    {
+        public FrmPhieuChotCa()
+        {
+            InitializeComponent();
+        }
+
+        ChiTietPhieuChotCa chiTietPhieuChotCa = new ChiTietPhieuChotCa();
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            if (cbMaNhanVien.SelectedItem == null ||
+                tbThoiGian.Text == null ||
+                tbTienThucTe.Text == null ||
+                tbTienThucTe.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ các trường!");
+            } else
+            {
+                // Lay danh sach hoa don ban
+                string hoaDonBanQuery = String.Format("SELECT TongTien FROM HoaDonBan WHERE MaNhanVien='{0}' AND NgayBan='{1}'",
+                    cbMaNhanVien.SelectedValue, tbThoiGian.Text);
+                SqlDataAdapter hdbSqlData = new SqlDataAdapter(hoaDonBanQuery, Function.conn);
+                DataTable hdbTable = new DataTable();
+                hdbSqlData.Fill(hdbTable);
+                int tongTien = 0;
+                foreach (DataRow row in hdbTable.Rows)
+                {
+                    tongTien += Int32.Parse(row[0].ToString());
+                }
+                chiTietPhieuChotCa = new ChiTietPhieuChotCa();
+                chiTietPhieuChotCa.TienHeThong = tongTien;
+                chiTietPhieuChotCa.ThoiGian = tbThoiGian.Text;
+                chiTietPhieuChotCa.CaLamViec = cbCaLamViec.Text;
+                chiTietPhieuChotCa.TienThucTe = Convert.ToInt32(tbTienThucTe.Text);
+                chiTietPhieuChotCa.ChenhLech = chiTietPhieuChotCa.TienHeThong - chiTietPhieuChotCa.TienThucTe;
+                List<ChiTietPhieuChotCa> danhSachChiTietPhieuChotCa = new List<ChiTietPhieuChotCa>();
+                danhSachChiTietPhieuChotCa.Add(chiTietPhieuChotCa);
+                gridViewChenhLech.DataSource = danhSachChiTietPhieuChotCa;
+                gridViewChenhLech.Columns[0].HeaderText = "Thời Gian";
+                gridViewChenhLech.Columns[1].HeaderText = "Ca Làm Việc";
+                gridViewChenhLech.Columns[2].HeaderText = "Tiền Thực Tế";
+                gridViewChenhLech.Columns[3].HeaderText = "Tiền Hệ Thống";
+                gridViewChenhLech.Columns[4].HeaderText = "Chênh Lệch";
+            }
+        }
+
+        DataTable nhanVienTable = new DataTable();
+        DataTable caLamViecTable = new DataTable();
+        private void FrmPhieuChotCa_Load(object sender, EventArgs e)
+        {
+            // Lay danh sach nhan vien
+            string nhanVienQuery = "SELECT MaNhanVien, TenNhanVien FROM NhanVien";
+            SqlDataAdapter nhanVienSqlData = new SqlDataAdapter(nhanVienQuery, Function.conn);
+            nhanVienSqlData.Fill(nhanVienTable);
+            cbMaNhanVien.DataSource = nhanVienTable;
+            cbMaNhanVien.ValueMember = nhanVienTable.Columns[0].ColumnName;
+            cbMaNhanVien.DisplayMember = nhanVienTable.Columns[0].ColumnName;
+            cbMaNhanVien.SelectedItem = null;
+
+            // Lay danh sach ca lam viec
+            string caLamViecQuery = "SELECT TenCaLamViec FROM CaLamViec";
+            SqlDataAdapter caLamViecSqlData = new SqlDataAdapter(caLamViecQuery, Function.conn);
+            caLamViecSqlData.Fill(caLamViecTable);
+            cbCaLamViec.DataSource = caLamViecTable;
+            cbCaLamViec.ValueMember = caLamViecTable.Columns[0].ColumnName;
+            cbCaLamViec.DisplayMember = caLamViecTable.Columns[0].ColumnName;
+            cbCaLamViec.SelectedItem = null;
+
+            tbThoiGian.Text = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        private void cbMaNhanVien_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbMaNhanVien.SelectedItem == null)
+            {
+                tbTenNhanVien.Text = "";
+            }
+            else
+            {
+                tbTenNhanVien.Text = nhanVienTable.Rows[cbMaNhanVien.SelectedIndex].ItemArray[1].ToString();
+            }
+        }
+
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            COMExcel.Application exApp = new COMExcel.Application();
+            COMExcel.Workbook exBook; //Trong 1 chương trình Excel có nhiều Workbook
+            COMExcel.Worksheet exSheet; //Trong 1 Workbook có nhiều Worksheet
+            COMExcel.Range exRange;
+            string sql;
+            DataTable tblThongtinHD;
+            exBook = exApp.Workbooks.Add(COMExcel.XlWBATemplate.xlWBATWorksheet);
+            exSheet = (COMExcel.Worksheet)exBook.Worksheets[1];
+            // Định dạng chung
+            exRange = (COMExcel.Range)exSheet.Cells[1, 1];
+            exRange.Range["A1:G50"].Font.Name = "Times new roman";
+            exRange.Range["A1:D3"].Font.Size = 10;
+            exRange.Range["A1:D3"].Font.Name = "Times new roman";
+            exRange.Range["A1:G3"].Font.Bold = true;
+            exRange.Range["A1:D3"].Font.ColorIndex = 5; //Màu xanh da trời
+            exRange.Range["A1:A1"].ColumnWidth = 7;
+            exRange.Range["B1:B1"].ColumnWidth = 25;
+            exRange.Range["A1:D1"].MergeCells = true;
+            exRange.Range["A1:D1"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A2:D2"].MergeCells = true;
+            exRange.Range["A2:D2"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A2:D2"].Value = "72 Khương Trung - Thanh Xuân - Hà Nội";
+            exRange.Range["A3:D3"].MergeCells = true;
+            exRange.Range["A3:D3"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A3:D3"].Value = "Điện thoại: (04)37562222";
+            exRange.Range["A7:A7"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["B7:B7"].Value = "Tên nhân viên: ";
+            exRange.Range["C7:D7"].MergeCells = true;
+            exRange.Range["C7:D7"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignLeft;
+            exRange.Range["C7:D7"].Value = tbTenNhanVien.Text;
+            exRange.Range["A5:D5"].Font.Size = 16;
+            exRange.Range["A5:E5"].Font.Name = "Times new roman";
+            exRange.Range["A5:D5"].Font.Bold = true;
+            exRange.Range["A5:D5"].Font.ColorIndex = 3; //Màu đỏ
+            exRange.Range["A5:D5"].MergeCells = true;
+            exRange.Range["A5:D5"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["A5:D5"].Value = "PHIẾU CHỐT CA";
+            // Biểu diễn thông tin chung của phieu chot ca
+            // Ngay
+            DateTime d = DateTime.Now;
+            exRange.Range["A1:D1"].Value = "Hà Nội, ngày " + d.Day + " tháng " + d.Month + " năm " + d.Year;
+            exRange.Range["B6:C9"].Font.Size = 12;
+            exRange.Range["B6:C9"].Font.Name = "Times new roman";
+            exRange.Range["B6:B6"].Value = "Mã Nhân Viên:";
+            exRange.Range["C6:D6"].MergeCells = true;
+            exRange.Range["C6:D6"].Value = cbMaNhanVien.SelectedValue;
+            //Tạo dòng tiêu đề bảng
+            exRange.Range["A8:G8"].Font.Bold = true;
+            exRange.Range["A8:E8"].HorizontalAlignment = COMExcel.XlHAlign.xlHAlignCenter;
+            exRange.Range["C8:E8"].ColumnWidth = 12;
+            exRange.Range["A8:A8"].Value = "Thời Gian";
+            exRange.Range["B8:B8"].Value = "Tiền Thực Tế";
+            exRange.Range["C8:C8"].Value = "Tiền Hệ Thống";
+            exRange.Range["D8:D8"].Value = "Chênh Lệch";
+            //Điền số thứ tự vào cột 1 từ dòng 4
+            exSheet.Cells[9, 1] = chiTietPhieuChotCa.ThoiGian;
+            exSheet.Cells[9, 2] = chiTietPhieuChotCa.TienThucTe;
+            exSheet.Cells[9, 3] = chiTietPhieuChotCa.TienHeThong;
+            exSheet.Cells[9, 4] = chiTietPhieuChotCa.ChenhLech;
+            exRange.Font.Bold = true;
+            exRange = (COMExcel.Range)exSheet.Cells[1, 12]; //Ô A1 
+            exRange.Range["A6:C6"].MergeCells = true;
+            exRange.Range["A6:C6"].Font.Italic = true;
+            exSheet.Name = "PHIẾU CHỐT CA";
+            exApp.Visible = true;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            // Xoa tat ca
+            cbMaNhanVien.SelectedItem = null;
+            cbCaLamViec.SelectedItem = null;
+            tbTenNhanVien.Text = null;
+            tbThoiGian.Text = null;
+            tbTienThucTe.Text= "0";
+        }
+    }
+}
